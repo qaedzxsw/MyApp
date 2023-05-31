3bdae2c1243f88aa05289a2ffda50b5f50bb223d
student <qaedzxsw@gmail.com>
2023-05-31 10:17:46 +0800

    v.2

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.myapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import com.example.myapp.ui.theme.MyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +27,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
-
+                    AuthorInfo()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun AuthorInfo() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "作者：資工四B 賴威銡")
+        Image(
+            painter = painterResource(R.drawable.map),
+            contentDescription = "Map Image"
+        )
     }
 }
 
@@ -39,6 +58,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MyAppTheme {
-        Greeting("Android")
+        AuthorInfo()
     }
 }
